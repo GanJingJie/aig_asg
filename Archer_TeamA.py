61992a694873985e06758b4212582eb9bcf09684
GanJingJie <93971349+GanJingJie@users.noreply.github.com>
2023-01-25 22:20:46 +0800

    Added Fleeing state

--- Archer_TeamA.py
+++ Archer_TeamA.py
@@ -8,6 +8,7 @@ from State import *
 
 ## declare global variable
 choice = 0
+level = 0
 
 class Archer_HalJordan(Character):
 
@@ -30,10 +31,12 @@ class Archer_HalJordan(Character):
         seeking_state = ArcherStateSeeking_HalJordan(self)
         attacking_state = ArcherStateAttacking_HalJordan(self)
         ko_state = ArcherStateKO_HalJordan(self)
+        fleeing_state = ArcherStateFleeing_HalJordan(self)
 
         self.brain.add_state(seeking_state)
         self.brain.add_state(attacking_state)
         self.brain.add_state(ko_state)
+        self.brain.add_state(fleeing_state)
 
         self.brain.set_state("seeking")
 
@@ -48,14 +51,19 @@ class Archer_HalJordan(Character):
 
         ## levelling up in order
         global choice
+        global level
 
-        level_up_stats = ["projectile range", "speed", "ranged damage"]
+        level_up_stats = ["ranged damage", "ranged cooldown"]
         if self.can_level_up():
-            if choice == 3:
+            if choice == 2:
                 choice = 0
             self.level_up(level_up_stats[choice])
             choice += 1
+            level += 1
 
+        if self.brain.active_state.name == "fleeing":
+            if self.current_hp < self.max_hp:
+                self.heal()
 
 class ArcherStateSeeking_HalJordan(State):
 
@@ -63,8 +71,12 @@ class ArcherStateSeeking_HalJordan(State):
 
         State.__init__(self, "seeking")
         self.archer = archer
+        global level
 
-        self.archer.path_graph = self.archer.world.paths[randint(0, len(self.archer.world.paths) - 1)]
+        if level >= 4:
+            self.archer.path_graph = self.archer.world.paths[3]
+        else:
+            self.archer.path_graph = self.archer.world.paths[0]
 
 
     def do_actions(self):
@@ -84,9 +96,6 @@ class ArcherStateSeeking_HalJordan(State):
             if opponent_distance <= self.archer.min_target_distance:
                     self.archer.target = nearest_opponent
                     return "attacking"
-            if opponent_distance < self.archer.min_target_distance:
-                    #back up
-                    return 
         
         if (self.archer.position - self.archer.move_target.position).length() < 8:
 
@@ -125,11 +134,21 @@ class ArcherStateAttacking_HalJordan(State):
 
     def do_actions(self):
 
-        opponent_distance = (self.archer.position - self.archer.target.position).length()
+        archer = self.archer.position
+        archerSpeed = self.archer.target.position - self.archer.position
+        nearestEntity = self.archer.world.get_nearest_opponent(self.archer)
+
+        #calculate the other point of line
+        
+
+        opponent_distance = (archer - self.archer.target.position).length()
+        
 
         # opponent within range
         if opponent_distance <= self.archer.min_target_distance:
-            self.archer.velocity = Vector2(0, 0)
+            self.archer.velocity = Vector2(0,0)
+            #if intersect(archer,):
+                #detect if movement is required
             if self.archer.current_ranged_cooldown <= 0:
                 self.archer.ranged_attack(self.archer.target.position)
 
@@ -141,17 +160,22 @@ class ArcherStateAttacking_HalJordan(State):
 
 
     def check_conditions(self):
+        opponent_distance = (self.archer.position - self.archer.target.position).length()
+
+        if opponent_distance <= (self.archer.min_target_distance / 3) or self.archer.current_hp < self.archer.max_hp * 0.5:
+            return "fleeing"
 
         # target is gone
         if self.archer.world.get(self.archer.target.id) is None or self.archer.target.ko:
             self.archer.target = None
             return "seeking"
-
+        
         return None
 
     def entry_actions(self):
 
         return None
+
 
 
 class ArcherStateKO_HalJordan(State):
@@ -172,7 +196,10 @@ class ArcherStateKO_HalJordan(State):
         if self.archer.current_respawn_time <= 0:
             self.archer.current_respawn_time = self.archer.respawn_time
             self.archer.ko = False
-            self.archer.path_graph = self.archer.world.paths[randint(0, len(self.archer.world.paths)-1)]
+            if level >= 4:
+                self.archer.path_graph = self.archer.world.paths[3]
+            else:
+                self.archer.path_graph = self.archer.world.paths[0]
             return "seeking"
             
         return None
@@ -185,3 +212,40 @@ class ArcherStateKO_HalJordan(State):
         self.archer.target = None
 
         return None
+
+class ArcherStateFleeing_HalJordan(State):
+
+    def __init__(self,archer):
+
+        State.__init__(self, "fleeing")
+        self.archer = archer
+
+    def do_actions(self):
+
+        self.archer.velocity = self.archer.move_target.position - self.archer.position
+        if self.archer.velocity.length() > 0:
+            self.archer.velocity.normalize_ip()
+            self.archer.velocity *= self.archer.maxSpeed
+        
+
+    def check_conditions(self):
+        
+        # target safe
+         if self.archer.world.get(self.archer.target.id) is None or self.archer.target.ko:
+            self.archer.target = None
+            return "attacking"
+
+    def entry_actions(self):
+        
+        flee_direction = (self.archer.position - self.archer.target.position).normalize()
+        flee_distance = -1000
+        self.archer.move_target.position = self.archer.position - flee_direction * flee_distance
+
+# Checking if vectors intersect
+def ccw(A,B,C):
+    
+    return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x)
+
+def intersect(A,B,C,D):
+
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
